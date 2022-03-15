1e94247e8ff490e8a9fec224999be4a0519c9268
Parth <Parth@WIN22H1>
2022-03-15 18:22:33 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GeneratedPasswordBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.RadioLength08 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.RadioLength16 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.RadioLength20 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.BtnGenerate = new Guna.UI2.WinForms.Guna2Button();
+            this.BtnCopy = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TrayIconStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RoundCorners = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.TrayIconStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GeneratedPasswordBox
+            // 
+            this.GeneratedPasswordBox.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.GeneratedPasswordBox.BorderRadius = 16;
+            this.GeneratedPasswordBox.BorderThickness = 3;
+            this.GeneratedPasswordBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.GeneratedPasswordBox.DefaultText = "";
+            this.GeneratedPasswordBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.GeneratedPasswordBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.GeneratedPasswordBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.GeneratedPasswordBox.DisabledState.Parent = this.GeneratedPasswordBox;
+            this.GeneratedPasswordBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.GeneratedPasswordBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.GeneratedPasswordBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.GeneratedPasswordBox.FocusedState.Parent = this.GeneratedPasswordBox;
+            this.GeneratedPasswordBox.Font = new System.Drawing.Font("Gotham", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.GeneratedPasswordBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.GeneratedPasswordBox.HoverState.Parent = this.GeneratedPasswordBox;
+            this.GeneratedPasswordBox.Location = new System.Drawing.Point(12, 12);
+            this.GeneratedPasswordBox.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.GeneratedPasswordBox.Name = "GeneratedPasswordBox";
+            this.GeneratedPasswordBox.PasswordChar = '\0';
+            this.GeneratedPasswordBox.PlaceholderText = "";
+            this.GeneratedPasswordBox.ReadOnly = true;
+            this.GeneratedPasswordBox.SelectedText = "";
+            this.GeneratedPasswordBox.ShadowDecoration.Parent = this.GeneratedPasswordBox;
+            this.GeneratedPasswordBox.Size = new System.Drawing.Size(376, 47);
+            this.GeneratedPasswordBox.TabIndex = 0;
+            this.GeneratedPasswordBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // RadioLength08
+            // 
+            this.RadioLength08.Checked = true;
+            this.RadioLength08.CheckedState.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.RadioLength08.CheckedState.BorderThickness = 0;
+            this.RadioLength08.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength08.CheckedState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength08.CheckedState.Parent = this.RadioLength08;
+            this.RadioLength08.Location = new System.Drawing.Point(12, 65);
+            this.RadioLength08.Name = "RadioLength08";
+            this.RadioLength08.ShadowDecoration.Parent = this.RadioLength08;
+            this.RadioLength08.Size = new System.Drawing.Size(20, 20);
+            this.RadioLength08.TabIndex = 1;
+            this.RadioLength08.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength08.UncheckedState.BorderThickness = 2;
+            this.RadioLength08.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.RadioLength08.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            this.RadioLength08.UncheckedState.Parent = this.RadioLength08;
+            // 
+            // RadioLength16
+            // 
+            this.RadioLength16.CheckedState.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.RadioLength16.CheckedState.BorderThickness = 0;
+            this.RadioLength16.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength16.CheckedState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength16.CheckedState.Parent = this.RadioLength16;
+            this.RadioLength16.Location = new System.Drawing.Point(12, 91);
+            this.RadioLength16.Name = "RadioLength16";
+            this.RadioLength16.ShadowDecoration.Parent = this.RadioLength16;
+            this.RadioLength16.Size = new System.Drawing.Size(20, 20);
+            this.RadioLength16.TabIndex = 1;
+            this.RadioLength16.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength16.UncheckedState.BorderThickness = 2;
+            this.RadioLength16.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.RadioLength16.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            this.RadioLength16.UncheckedState.Parent = this.RadioLength16;
+            // 
+            // RadioLength20
+            // 
+            this.RadioLength20.CheckedState.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.RadioLength20.CheckedState.BorderThickness = 0;
+            this.RadioLength20.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength20.CheckedState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength20.CheckedState.Parent = this.RadioLength20;
+            this.RadioLength20.Location = new System.Drawing.Point(12, 117);
+            this.RadioLength20.Name = "RadioLength20";
+            this.RadioLength20.ShadowDecoration.Parent = this.RadioLength20;
+            this.RadioLength20.Size = new System.Drawing.Size(20, 20);
+            this.RadioLength20.TabIndex = 1;
+            this.RadioLength20.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.RadioLength20.UncheckedState.BorderThickness = 2;
+            this.RadioLength20.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.RadioLength20.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            this.RadioLength20.UncheckedState.Parent = this.RadioLength20;
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.BtnGenerate.BorderRadius = 20;
+            this.BtnGenerate.BorderThickness = 3;
+            this.BtnGenerate.CheckedState.Parent = this.BtnGenerate;
+            this.BtnGenerate.CustomImages.Parent = this.BtnGenerate;
+            this.BtnGenerate.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BtnGenerate.Font = new System.Drawing.Font("Gotham", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.BtnGenerate.ForeColor = System.Drawing.Color.White;
+            this.BtnGenerate.HoverState.Parent = this.BtnGenerate;
+            this.BtnGenerate.Location = new System.Drawing.Point(12, 143);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.ShadowDecoration.Parent = this.BtnGenerate;
+            this.BtnGenerate.Size = new System.Drawing.Size(180, 45);
+            this.BtnGenerate.TabIndex = 2;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // BtnCopy
+            // 
+            this.BtnCopy.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.BtnCopy.BorderRadius = 20;
+            this.BtnCopy.BorderThickness = 3;
+            this.BtnCopy.CheckedState.Parent = this.BtnCopy;
+            this.BtnCopy.CustomImages.Parent = this.BtnCopy;
+            this.BtnCopy.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BtnCopy.Font = new System.Drawing.Font("Gotham", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.BtnCopy.ForeColor = System.Drawing.Color.White;
+            this.BtnCopy.HoverState.Parent = this.BtnCopy;
+            this.BtnCopy.Location = new System.Drawing.Point(208, 143);
+            this.BtnCopy.Name = "BtnCopy";
+            this.BtnCopy.ShadowDecoration.Parent = this.BtnCopy;
+            this.BtnCopy.Size = new System.Drawing.Size(180, 45);
+            this.BtnCopy.TabIndex = 2;
+            this.BtnCopy.Text = "Copy";
+            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Gotham", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(38, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "8 Character Length";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Gotham", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(38, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(350, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "16 Character Length";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Gotham", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(38, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(350, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "20 Character Length";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.ContextMenuStrip = this.TrayIconStrip;
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "Password Generator";
+            this.TrayIcon.Visible = true;
+            this.TrayIcon.Click += new System.EventHandler(this.TrayIcon_Click);
+            this.TrayIcon.DoubleClick += new System.EventHandler(this.TrayIcon_DoubleClick);
+            // 
+            // TrayIconStrip
+            // 
+            this.TrayIconStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.TrayIconStrip.Name = "TrayIconStrip";
+            this.TrayIconStrip.Size = new System.Drawing.Size(104, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // RoundCorners
+            // 
+            this.RoundCorners.BorderRadius = 20;
+            this.RoundCorners.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCopy);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.RadioLength20);
+            this.Controls.Add(this.RadioLength16);
+            this.Controls.Add(this.RadioLength08);
+            this.Controls.Add(this.GeneratedPasswordBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.TrayIconStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox GeneratedPasswordBox;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton RadioLength08;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton RadioLength16;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton RadioLength20;
+        private Guna.UI2.WinForms.Guna2Button BtnGenerate;
+        private Guna.UI2.WinForms.Guna2Button BtnCopy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+        private System.Windows.Forms.ContextMenuStrip TrayIconStrip;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private Guna.UI2.WinForms.Guna2Elipse RoundCorners;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        #region UI
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            RadioLength08.Checked = true;
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            RadioLength16.Checked = true;
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+            RadioLength20.Checked = true;
+        }
+
+        #endregion
+
+        #region PasswordGenerator
+
+        public void CodeParthGeneratedPassword(int Length)
+        {
+            const string PasswordCharacters = "abcdefghijklmnopqrstuvwxyz"+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"0123456789"+"!@#$%&()?";
+            StringBuilder GeneratedPassword = new StringBuilder();
+            Random Pass = new Random();
+            while (0 < Length--)
+            {
+                GeneratedPassword.Append(PasswordCharacters[Pass.Next(PasswordCharacters.Length)]);
+            }
+            GeneratedPasswordBox.Text = GeneratedPassword.ToString();
+        }
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(GeneratedPasswordBox.Text);
+            GeneratedPasswordBox.Text = null;
+        }
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            if (RadioLength08.Checked == true)
+            {
+                CodeParthGeneratedPassword(8);
+            }
+            else if (RadioLength16.Checked == true)
+            {
+                CodeParthGeneratedPassword(16);
+            }
+            else if (RadioLength20.Checked == true)
+            {
+                CodeParthGeneratedPassword(20);
+            }
+            else
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region TrayIcon
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var desktopWorkingArea = Screen.PrimaryScreen.WorkingArea;
+            this.Left = desktopWorkingArea.Right - 415;
+            this.Top = desktopWorkingArea.Bottom - 215;
+        }
+
+        private void TrayIcon_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void TrayIcon_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                TrayIcon.Visible = true;
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #endregion
+    }
+}
